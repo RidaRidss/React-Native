f992870c7f8fcf3d8040a65930caaab697947764
rida <rida_rocks12@yahoo.com>
2017-10-10 17:38:50 +0500

    clean history

--- src/container/SignUp/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
-import styles from "./styles";
-
-export default class SignUp extends Component {
-  render() {
-    return <View />;
-  }
-}
